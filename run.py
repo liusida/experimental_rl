7b56999729a36263f3b60b350b957d1c6276c1eb
sida <sliu1@uvm.edu>
2021-04-02 11:25:41 -0400

    FirstTry

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ parser.add_argument("--total_timesteps", type=float, default=1e8, help="Total tr
 parser.add_argument("--num_venvs", type=int, default=16, help="Number of vectorized environments.")
 parser.add_argument("--seed", type=int, default=0)
 
+parser.add_argument("--exp_name", type=str, default="Default", help="Name of the experiment. So we can group them on WandB.")
 parser.add_argument("--extractor", type=str, default="TwoMlpExtractor", help="Specify different extractor. The main treatment of the experiment.")
 parser.add_argument("--env_id", type=str, default="HopperBulletEnv-v0", help="Specify different environment. The secondary treatment of the experiment.")
 
