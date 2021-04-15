369e280b55ba05b5878515dac2627567894dbdb0
sida <sliu1@uvm.edu>
2021-04-15 15:06:50 -0400

    auto-commit via vscode

--- erl/runs.py
+++ erl/runs.py
@@ -1,6 +1,5 @@
 import torch as th
 
-import erl.features_extractors.cs253 as extractors
 from erl.customized_agents.multi_extractor import MultiExtractor
 from erl.experiments.cs253.multimodule_exp import MultiModuleExp
 
@@ -9,6 +8,22 @@ def run_current_exp(args):
     run_cs253(args)
 
 def run_cs253(args):
-    extractor, extractor_kwargs = extractors.get(args.extractor)
+    extractor, extractor_kwargs = get_kwargs(args.extractor_kwargs)
     assert extractor==MultiExtractor
     MultiModuleExp(env_id=args.env_id, features_extractor_kwargs=extractor_kwargs, args=args).train()
+
+def get_kwargs(str_kwargs):
+    """ Get class and kwargs from a string of classname
+    For example a classname can be: "SomeClass:i=1&j=2"
+    and we should return the class of SomeClass, and a dictionary of {"i":1, "j":2}
+    
+    Note: The value of arguments should always be integers.
+    """
+    list_kwargs = str_kwargs.split(",")
+    kwargs = {}
+    for k in list_kwargs:
+        if k.find("=")==-1:
+            continue
+        key, value = k.split("=")
+        kwargs[key] = int(value)
+    return kwargs
