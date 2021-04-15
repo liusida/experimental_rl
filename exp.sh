00eedde0d09090be78662aeb7b8ed7d7dafb18c7
sida <sliu1@uvm.edu>
2021-04-15 14:23:10 -0400

    save

--- exp.sh
+++ exp.sh
@@ -6,6 +6,15 @@ git pull
 # 2021-04-15
 if true
 then
+    exp_name="OnlyRNN"
+    for seed in 0 1 2
+    do
+        sbatch -J $exp_name deepgreen.sh python run.py --cuda --exp_name=$exp_name --exp_group=rnns --env_id=HopperBulletEnv-v0 --extractor=MultiLSTMExtractor:m=1 --num_envs=16 --total_timesteps=3e6 --seed=$seed
+    done
+fi
+
+if false
+then
     exp_name="ImplementingRNN"
     for seed in 0 1 2
     do
