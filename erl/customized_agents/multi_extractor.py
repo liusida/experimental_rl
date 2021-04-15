e94e110984b6cda7af9e2ced11de7a8a2fb2249b
sida <sliu1@uvm.edu>
2021-04-15 15:14:40 -0400

    auto-commit via vscode

--- erl/customized_agents/multi_extractor.py
+++ erl/customized_agents/multi_extractor.py
@@ -44,7 +44,7 @@ class MultiExtractor(BaseFeaturesExtractor):
         
         self.final_layer_size = 64  # without n_input
         # check power of 2: https://stackoverflow.com/questions/57025836/how-to-check-if-a-given-number-is-a-power-of-two
-        assert (self.num_parallel_sum & (self.num_parallel_sum-1) == 0) and self.num_parallel_sum != 0, "num_parallel_sum is not power of 2"
+        assert (self.num_parallel_sum & (self.num_parallel_sum-1) == 0) or self.num_parallel_sum == 0, "num_parallel_sum is not power of 2"
         assert self.num_parallel_sum <= self.final_layer_size, "num_parallel_sum is too large"
 
         self.size_per_module = int(self.final_layer_size / self.num_parallel_sum)  # this is why we need m to be power of 2
