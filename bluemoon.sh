f6478f1fbd839464a50070fb39df33ffc948832b
sida <sliu1@uvm.edu>
2021-04-15 12:40:03 -0400

    save

--- bluemoon.sh
+++ bluemoon.sh
@@ -6,9 +6,10 @@
 # sbatch bluemoon.sh python run.py --total_timesteps=1e8
 
 #SBATCH --partition bluemoon
-#SBATCH --mem 24G
+#SBATCH --mem 2G
 #SBATCH -c 1
 #SBATCH --output /gpfs2/scratch/sliu1/slurm.out/%j
+#SBATCH --cores-per-socket 7
 
 cd ${SLURM_SUBMIT_DIR}
 
