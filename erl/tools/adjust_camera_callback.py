3f97745ca09e833e4b14ea3c51afb4e646ad0e09
sida <sliu1@uvm.edu>
2021-04-15 13:40:03 -0400

    auto-commit via vscode

--- erl/tools/adjust_camera_callback.py
+++ erl/tools/adjust_camera_callback.py
@@ -2,7 +2,7 @@ from stable_baselines3.common.callbacks import EventCallback
 
 class AdjustCameraCallback(EventCallback):
     def _on_step(self) -> bool:
-        self.camera_simpy_follow_robot()
+        self.camera_simpy_follow_robot(rotate=False)
         return super()._on_step()
 
 
