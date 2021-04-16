2d54689a229188116909cd7a06d80383ac9be64d
sida <sliu1@uvm.edu>
2021-04-15 22:47:06 -0400

    auto-commit via vscode

--- erl/tools/debug_callback.py
+++ erl/tools/debug_callback.py
@@ -2,8 +2,14 @@ import numpy as np
 
 from stable_baselines3.common.callbacks import EventCallback
 
+import wandb
+
 class DebugCallback(EventCallback):
     def _on_step(self) -> bool:
         s = np.sum(self.locals["actions"])
+        wandb.log({
+            "debug/sum_actions": s,
+            "step": self.num_timesteps
+        })
         print("debug> actions> ", s)
         return super()._on_step()
