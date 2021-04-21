35a9403eb9341c1e81ed463cc1271d26d89d0a5a
sida <sliu1@uvm.edu>
2021-04-20 23:55:32 -0400

    auto-commit via vscode

--- erl/envs/hopper_mask.py
+++ erl/envs/hopper_mask.py
@@ -4,5 +4,8 @@ from pybullet_envs.gym_locomotion_envs import HopperBulletEnv
 class HopperMaskEnv(HopperBulletEnv):
     def step(self, action):
         obs, reward, done, info = super().step(action)
-        obs = np.zeros_like(obs)
+        # obs = np.zeros_like(obs)
+        obs[10] = 0
+        obs[12] = 0
+        obs[14] = 0
         return obs, reward, done, info
