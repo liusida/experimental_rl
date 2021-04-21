06184ba225eb25951a330e17d53d7797f9ce40ee
sida <sliu1@uvm.edu>
2021-04-20 22:27:36 -0400

    auto-commit via vscode

--- erl/envs/walker2d_only_vision.py
+++ erl/envs/walker2d_only_vision.py
@@ -5,13 +5,16 @@ import pybullet_envs
 from pybullet_envs.gym_locomotion_envs import Walker2DBulletEnv, WalkerBaseBulletEnv
 from pybullet_envs.robot_locomotors import Walker2D, WalkerBase
 
+
 def debug_imshow(img):
     import matplotlib.pyplot as plt
     plt.imshow(img)
     plt.show()
 
+
 class Walker2DOnlyVision(Walker2D):
     """ a third person view. like controling a game. """
+
     def __init__(self):
         self.p = None
         self.camera_width = 64
@@ -19,7 +22,7 @@ class Walker2DOnlyVision(Walker2D):
         self.camera_channel = 3
         self.gravity = -9.8
         super().__init__()
-        
+
         # Reset the robot XML file:
         total_joints = 8
         WalkerBase.__init__(self,
@@ -29,8 +32,8 @@ class Walker2DOnlyVision(Walker2D):
                             # 10 real numbers of basic information
                             # each joint has 2 real numbers (position, velocity)
                             # WxHxC from camera image
-                            # obs_dim=10 + total_joints*2 + self.camera_width*self.camera_height*self.camera_channel, 
-                            obs_dim=self.camera_width*self.camera_height*self.camera_channel, 
+                            # obs_dim=10 + total_joints*2 + self.camera_width*self.camera_height*self.camera_channel,
+                            obs_dim=self.camera_width*self.camera_height*self.camera_channel,
                             power=0.40)
 
     def get_camera_image(self):
@@ -43,30 +46,27 @@ class Walker2DOnlyVision(Walker2D):
             self.p = self.scene._p
             # Turn on the Debug GUI
             self.p.configureDebugVisualizer(self.p.COV_ENABLE_GUI, 1)
-            self.p.setGravity(0,0,self.gravity)
-
-            distance = 3
-            pitch = -30
-            yaw = 0
+            self.p.setGravity(0, 0, self.gravity)
 
             # Precalculate the projection matrix
             fov, aspect, nearplane, farplane = 30, 1.0, 0.01, 100
             self.projection_matrix = self.p.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
-            
+
             # Get Index
             self.robot_id = self.parts['camera_lens'].bodies[0]
             self.camera_lens_id = self.parts['camera_lens'].bodyPartIndex
             # Change the camera_lens to white, just to make sure the camera is mounted on the right body part
-            self.p.changeVisualShape(self.robot_id, self.camera_lens_id,rgbaColor=[1,1,1,1])
+            self.p.changeVisualShape(self.robot_id, self.camera_lens_id, rgbaColor=[1, 1, 1, 1])
 
         # Why I need to '*1.1' here?
         _current_x = self.body_xyz[0] * 1.1
         _current_y = self.body_xyz[1] * 1.1
 
         lookat = [_current_x, _current_y, 0.7]
-        
-        ret = self.p.getDebugVisualizerCamera()
-        view_matrix,projection_matrix = ret[1],ret[2]
+
+        # ret = self.p.getDebugVisualizerCamera()
+        # view_matrix, projection_matrix = ret[1], ret[2]
+        view_matrix = self.p.computeViewMatrixFromYawPitchRoll(distance=3, yaw = 0, pitch=-30, roll=0, upAxisIndex=2)
         _, _, rgbPixels, depthPixels, segmentationMaskBuffer = self.p.getCameraImage(self.camera_width, self.camera_height, view_matrix, self.projection_matrix)
         return rgbPixels
 
@@ -78,11 +78,11 @@ class Walker2DOnlyVision(Walker2D):
         camera_img = self.get_camera_image()
         # remove the Alpha channel to save space
         # camera_img: 196608 dimensions (256x256x3)
-        camera_img = camera_img[:,:,:3] / 255.0
-        #(WxHxC) => (CxHxW)
+        camera_img = camera_img[:, :, :3] / 255.0
+        # (WxHxC) => (CxHxW)
         # camera_img = np.swapaxes(camera_img, 0, 2)
         camera_img = np.rollaxis(camera_img, -1)
-        
+
         # Flatten the observation and reconstruct them in the feature extractor.
         # Hard coded warning. see vae_extractor.py
         # basic_state = [:46] and camera_img = obs[46:].view(3,256,256)
@@ -91,15 +91,16 @@ class Walker2DOnlyVision(Walker2D):
         obs = camera_img
         return obs
 
+
 class Walker2DOnlyVisionEnv(Walker2DBulletEnv):
     def __init__(self, render=False):
         self.robot = Walker2DOnlyVision()
         WalkerBaseBulletEnv.__init__(self, self.robot, render)
-    
+
     def reset(self):
         self.episodic_steps = 0
         return super().reset()
-    
+
     def step(self, action):
         self.episodic_steps += 1
         return super().step(action)
